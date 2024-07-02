8b447e7c77c2e9e85666255c129a8dd14c19ec39
PawanJoshi42 <pawanratala42@gmail.com>
2024-07-02 13:08:23 +0545

    texas python class week first

---
+++ hello.py
@@ -0,0 +1,224 @@
+# print('hello world')
+
+#This is a string
+first_name = 'Dilip'
+
+# if first_name == "Pawan": # use of == (it is used to compare)
+#     print("You are eligible to enter")
+
+# if first_name != "Pawan": # use of != (it is used to compare if is a correct data or nots)
+#     print("You are eligible to enter")
+
+# #This is a integer
+# number = 20
+
+# #This is a float
+# decimal_number = 20.5
+
+# #boolean
+# is_this_texas = True
+
+# #to find the data type
+# print(type(first_name))
+# print(type(is_this_texas))
+
+# #to take user input 
+# user_name = input("WHAT IS YOUR NAME:?")
+# user_age = input("WHAT IS YOUR AGE:?")
+
+# print(user_name)
+# print("Your age is " + str(user_age))
+
+# #Writing a program to create a app that can calculate user age by its date of birth 
+
+# year_of_birth = input("Enter the year of birth")
+# current_year = 2024
+
+# current_age = current_year - int(year_of_birth)
+
+# print("Your current age is: "+str(current_age))
+
+
+# #Using condition 
+
+# if current_age < 18:
+#     print("You are not eligible")
+# else:
+#     print("You are eligible")
+
+
+# #python variable assign 
+# name = "John"
+# age = 23
+# gender = "M"
+# pending_fees = True
+
+# print(name,age,gender,pending_fees)
+# print("Type of name",type(name))
+# print("Type of age",type(age))
+# print("Type of gender",type(gender))
+# print("Type of pending_fees",type(pending_fees))
+
+
+# # Arithemetic Operators
+
+# num1 = 25
+# num2 = 25
+
+# print(num1+num2)    #addition
+# print(num1-num2)    #subtraction
+# print(num1*num2)    #multiplication
+# print(num1/num2)    #division
+# print(num1%num2)    #modulus
+# print(num1**num2)   #exponential
+# print(num1//num2)   #floor division
+
+
+#Assignment Operators
+
+# =(Assign)
+# x=10
+# print("x=",x)
+
+# # +=(Add and assign)
+# x +=10
+# print("x += 5 ->",x)
+
+
+#Comparison Operators
+
+# num1 = 23
+# num2 = 24
+
+# print(num1 == num2) #Equal to
+# print(num1 > num2)  #Greater than
+# print(num1 != num2) #Not Equal to
+# print(num1 <= num2) #Less Than oe equals to
+
+
+#Logical Operators
+
+# a = True
+# b = False
+
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+
+
+#Identity Operators
+
+# a = 100
+# b = 100
+# c = 104
+
+# print( a is b) # true 
+# print( a is c) # false
+
+
+# Membership Operators
+
+# fruit = "banana"
+# print("a" in fruit) #true because a is in fruit banana
+# print("z" in fruit) #false because b in not in the fruit banana
+
+#Operator precendance
+
+# equ = 2 + 3 * 6 / (4**5)
+
+
+#Ternary Operators
+#checking maximum value
+# a,b = 10,20
+# min = a if a < b else b
+# print(min)
+
+# if statement
+# i = 10
+# if(i<12):
+#     print("10 is less than 12")
+
+# if else statement
+# if(10>5):
+#     print("Wrong")
+# else:
+#     print("10 is greater than 5")
+
+
+# Nested if 
+
+# x=41
+
+# if x>10:
+#     print("Above ten")
+# if x>20:
+#     print("and also above 20!")
+# else:
+#     print("but not above 20")
+
+
+# # if-elif ladder
+
+# i = 25
+# if(i == 10):
+#     print("i is 10")
+# elif(i == 15):
+#     print("i is 15")
+# elif( i == 20):
+#     print("i is 20")
+# else:
+#     print("i is above 20")
+
+
+#Shorthand if-else statement
+
+#Handling user input
+
+# user_name= input("What is your name? ")
+# print("Welcome "+str(user_name))
+
+
+
+#Assignment
+
+# score = float(input("Enter your score: "))
+# if score > 90:
+#     print("Your grade is A")
+# elif score > 75:
+#     print("Your grade is B")
+# elif score > 65:
+#     print("Your grade is C")
+# else:
+#     print("Your are failed")
+
+
+# Write a program to calculate the electricity bill(accept number of unit from user)
+#according to the following criteria:
+
+# unit = int(input("Enter your unit"))
+# if unit <= 100:
+#    print("No charge !! ")
+# elif unit <=200 and unit>100:
+#     print("Your total bill amount"+ str((unit-100)*5))
+# elif unit > 200:
+#     print("Your total bill amount"+ str(500+(unit-200)*10))
+
+
+#Loop condition 
+                  #For Loop
+                  #While Loop
+                  #Nested Loop
+
+
+#For loop example
+        # text="Pythonprograaming"
+        # for i in text:
+        # print(i)
+
+#For loop with example:
+fruits=["apple","banana","grapes"]
+for index in range(len(fruits)):
+ print(len(fruits))
+else:
+ print("Inside Else Block")
